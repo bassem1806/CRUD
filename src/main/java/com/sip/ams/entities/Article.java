0355afa42c5786b930c39740c726c6f911c97509
Bassem Ben dhief <bendhief.b@gmail.com>
2022-03-31 13:20:11 +0200

    2eme Entity

--- src/main/java/com/sip/ams/entities/Article.java
+++ src/main/java/com/sip/ams/entities/Article.java
@@ -1,41 +1,48 @@
 package com.sip.ams.entities;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotBlank;
 
-public class Article {
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
-	
+@Entity
+
+public class Article {
 	@Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
-    @NotBlank(message = "label is mandatory")
+    @NotBlank(message = "Label is mandatory")
     @Column(name = "label")
     private String label;
     
-    @NotBlank(message = "price is mandatory")
+    
+ 
     @Column(name = "price")
-    private String price;
+    private float price;
 
-    
-    
-    
-    
-    
-    
-    
-    
-	public long getId() {
-		return id;
-	}
+    public Article() {}
 
-	public void setId(long id) {
-		this.id = id;
-	}
+    public Article(String label, float price) {
+        this.price = price;
+        this.label = label;
+        }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
 
 	public String getLabel() {
 		return label;
@@ -45,16 +52,29 @@ public class Article {
 		this.label = label;
 	}
 
-	public String getPrice() {
+	public float getPrice() {
 		return price;
 	}
 
-	public void setPrice(String price) {
+	public void setPrice(float price) {
 		this.price = price;
 	}
-    
-    
-    
-  
 
+	
+	/**** Many To One ****/
+	
+@ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "provider_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Provider provider;
+	
+	
+	public Provider getProvider() {
+    	return provider;
+    }
+    
+    public void setProvider(Provider provider) {
+    	this.provider=provider;
+    }  
+    
 }
